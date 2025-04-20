867df6100fc752382974ff9285584c3072dc49fa
migue <miguel.hernandez011@gmail.com>
2025-04-19 19:36:00 -0600

    Added logging

--- Devices.py
+++ Devices.py
@@ -1,16 +1,21 @@
 import logging
 import socket
 
+import args
 from bacpypes.core import run, stop, deferred
+from bacpypes.debugging import ModuleLogger
 from bacpypes.local.device import LocalDeviceObject
 from bacpypes.service.device import WhoIsIAmServices
 from bacpypes.object import AnalogInputObject, BinaryInputObject
 from bacpypes.app import BIPSimpleApplication
 from bacpypes.pdu import Address
+from bacpypes.service.object import ReadWritePropertyServices, ReadWritePropertyMultipleServices
 from threading import Timer
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+_debug = 0
+_log = ModuleLogger(globals())
 
 
 def create_virtual_device(device_id=12345, port=47808):
@@ -19,7 +24,10 @@ def create_virtual_device(device_id=12345, port=47808):
         objectIdentifier= ("device", device_id),
         maxApduLengthAcepted = 1024,
         segmentationSupported = "segmentedBoth",
-        vendorIdentifier = 15
+        vendorIdentifier = 15,
+        vendorName="B612",
+        maxApduLengthAccepted=1024,
+
     )
 
     temp_sensor = AnalogInputObject(
@@ -40,12 +48,12 @@ def create_virtual_device(device_id=12345, port=47808):
     this_device.objectList.append(temp_sensor)
     this_device.objectList.append(fan_output)
 
-    # address = Address('192.168.1.100:47808')  # replace with your local IP
     address = Address(f"0.0.0.0:{port}")
     application = BIPSimpleApplication(this_device, address)
     application.add_object(temp_sensor)
     application.add_object(fan_output)
     application.add_capability(WhoIsIAmServices)
+    application.add_capability(ReadWritePropertyServices)
 
     return application, temp_sensor, fan_output
 
@@ -65,10 +73,32 @@ def logic_loop(temp_sensor, fan_output):
 
     Timer(5, deferred, [logic_loop, temp_sensor, fan_output]).start()
 
+def request(self, apdu):
+    if _debug: _log.debug("    - args: %r", args)
+    BIPSimpleApplication.request(self, apdu)
+
 
 if __name__ == "__main__":
-    app, temp, fan = create_virtual_device(device_id=12345, port=47808)
+    if _debug: _log.debug("initialization")
+    if _debug: _log.debug("    - args: %r", args)
 
-    deferred(logic_loop, temp, fan)
+    try:
+        # code goes here...
 
-    run()
+        _log.debug("initialization")
+        # code goes here...
+
+        _log.debug("running")
+        # code goes here...
+
+        app, temp, fan = create_virtual_device(device_id=12345, port=47808)
+
+        deferred(logic_loop, temp, fan)
+
+        run()
+
+
+    except Exception as e:
+        _log.exception("an error has occurred: %s", e)
+    finally:
+        _log.debug("finally")
